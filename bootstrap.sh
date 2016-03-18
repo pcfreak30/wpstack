bd6b39845e4da0eb7587d715e5c49713eee970ac
Derrick Hammer <derrick@derrickhammer.com>
2016-03-18 16:37:04 -0400

    *Call entrypoint.sh then kill apache

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Hack, entrypoint.sh calls apache, so we silently kill it and spawn out own in $@
+/entrypoint.sh apache2
+
+killall -9 apache2 &> /dev/null
+
 # If HOST_UID is passed, change the permissions
 if [ ! -z "$HOST_UID" ]; then
     usermod -u ${HOST_UID} www-data
