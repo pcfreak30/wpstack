df1bff3263b78fc44129c19984c269a1d8a4d51a
Derrick Hammer <derrick@derrickhammer.com>
2018-01-18 02:44:56 -0500

    *Override db home/siteurl with constant based on HTTP request header

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ set -e
 # Call entrypoint.sh
 docker-entrypoint.sh "$@"
 
+sed -i '/WP_DEBUG/a define("WP_SITEURL", "http://".$_SERVER["HTTP_HOST"]);' wp-config.php
+sed -i '/WP_DEBUG/a define("WP_HOME", "http://".$_SERVER["HTTP_HOST"]);' wp-config.php
+
 # If HOST_UID is passed, change the permissions
 if [ ! -z "$HOST_UID" ]; then
     usermod -u ${HOST_UID} www-data
